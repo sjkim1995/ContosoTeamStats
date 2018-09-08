60921012702d0dfe694ae62cdee3ebdf79ddc3d4
sjkim1995 <seki@microsoft.com>
2018-09-07 17:32:40 -0700

    Initial Commit

---
+++ ContosoTeamStats/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using StackExchange.Redis;
+
+namespace ContosoTeamStats.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        public ActionResult RedisCache()
+        {
+            ViewBag.Message = "A simple example with Azure Redis Cache on ASP.NET";
+
+            var lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                Console.WriteLine("Where the hell does this appear?!");
+                string cacheConnection = ConfigurationManager.AppSettings["CacheConnection"].ToString();
+                return ConnectionMultiplexer.Connect(cacheConnection);
+
+            });
+
+            IDatabase cache = lazyConnection.Value.GetDatabase();
+
+            // Simple PING command
+            ViewBag.command1 = "PING";
+            ViewBag.command1Result = cache.Execute(ViewBag.command1).ToString();
+
+            // Simple get and put of integral data types into the cache
+            ViewBag.command2 = "GET Message";
+            ViewBag.command2Result = cache.StringGet("Message").ToString();
+
+            ViewBag.command3 = "SET Message \"Hello! The cache is working from ASP .NET!\"";
+            ViewBag.command3Result = cache.StringSet("Message", ViewBag.command3).ToString();
+
+            //Demonstrate "SET" Message executed as expected
+            ViewBag.command4 = "GET Message";
+            ViewBag.command4result = cache.StringGet("Message").ToString();
+
+            // Get the client list, useful to see if connection list is growing...
+            ViewBag.command5 = "CLIENT LIST";
+            ViewBag.command5Result = cache.Execute("CLIENT", "LIST").ToString().Replace("id=", "\rid=");
+
+            lazyConnection.Value.Dispose();
+
+            return View();
+
+        }
+    }
+}
